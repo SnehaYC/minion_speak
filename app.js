49e94ab967d8836353f9b72f5765ce50e07261a1
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-03-09 19:32:03 +0530

    Done

---
+++ app.js
@@ -0,0 +1,23 @@
+var textInput = document.getElementById("input-area");
+var button = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output-area");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getRequiredURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+function errorHandler(error) {
+  alert("something wrong with server! try again after some time");
+}
+function clickhandler() {
+  var inputValue = textInput.value;
+  fetch(getRequiredURL(inputValue))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+button.addEventListener("click", clickhandler);
